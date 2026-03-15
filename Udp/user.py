482f7d43eca7e176180ab3fba86dec59a174ebee
Jude Lingan <judecohenlingan@gmail.com>
2026-03-15 05:59:27 -0400

    edited testing scripts
    updated test scripts

--- Udp/user.py
+++ Udp/user.py
@@ -1,14 +1,12 @@
 import socket
 
-SERVER_IP = "10.159.50.1"
-PORT = 12344
+SERVER_IP = "127.0.0.1"
+PORT = 12345
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
   s.connect((SERVER_IP, PORT))
   print("Connection Successful!")
-  s.sendall(b"Testing")
-  while True:
-    data = s.recv(1024)
-    print(data)
+  s.sendall(b"Testing\n")
+  s.sendall(b"test\n")
