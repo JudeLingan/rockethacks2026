f3d8678c9bf8c44e4d85aaffeb7ab353b88c4acd
Jude Lingan <judecohenlingan@gmail.com>
2026-03-15 07:12:30 -0400

    removed unnecessary files

--- Udp/client.py
+++ Udp/client.py
@@ -8,7 +8,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
   s.connect((SERVER_IP, PORT))
   print("Connection Successful!")
-  s.sendall(b"Testing")
+  #s.sendall(b"Testing")
   while True:
     data = s.recv(1024)
     print(data)
