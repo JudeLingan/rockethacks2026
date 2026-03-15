7f57e564dff85d2b3252a8ecf17d2ac3cf0808db
Jude Lingan <judecohenlingan@gmail.com>
2026-03-15 05:58:43 -0400

    backend relay working

--- Backend/src/main/java/reconbot/backend/UserHandler.java
+++ Backend/src/main/java/reconbot/backend/UserHandler.java
@@ -2,25 +2,43 @@ package reconbot.backend;
 
 import java.net.*;
 import java.io.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class ConnectionHandler {
+public class UserHandler {
 	private ServerSocket serverSocket;
 	private Socket clientSocket;
 	private PrintWriter out;
 	private BufferedReader in;
+	private AtomicInteger currentChar;
 
-	public void start(int port) {
+	public UserHandler() {
+		currentChar = new AtomicInteger();
+		currentChar.set('s');
+	}
+
+	public void start(int port, ConnectionHandler connectionHandler) {
 		try {
 			System.out.println("Connection Started");
 			serverSocket = new ServerSocket(port);
 			clientSocket = serverSocket.accept();
 			out = new PrintWriter(clientSocket.getOutputStream(), true);
 			in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			while (true) {
+				String nextLine = in.readLine();
+				if (nextLine == null) continue;
+				System.out.println(nextLine);
+				connectionHandler.getOut().println(nextLine);
+			}
 		}
 		catch (Exception e) {
 			System.out.println(e.getMessage());
 			System.exit(1);
 		}
+		System.out.println("init complete");
+	}
+
+	public char getCurrentChar() {
+		return (char)currentChar.get();
 	}
 
 	public void stop() {
@@ -34,9 +52,5 @@ public class ConnectionHandler {
 			System.out.println(e.getMessage());
 			System.exit(1);
 		}
-	}
-	
-	public PrintWriter getOut() {
-		return out;
 	}
 }
